d931e6068ea6c89c88cf0c7f43ac3160a22cd841
rishabhtripathi103 <rishabhtripathi103@gmail.com>
2020-07-21 18:36:01 +0530

    first commit

---
+++ src/containers/ContactUs/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+/**
+ * @author
+ * @function ContactUs
+ **/
+
+const ContactUs = (props) => {
+  return <div>Contact Us</div>;
+};
+
+export default ContactUs;
